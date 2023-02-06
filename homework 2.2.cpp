43b2146c4ece4ac7b8222db52eee2ed7c93c0ffa
Yakov Kontarook <kontarook@gmail.com>
2023-02-06 18:59:29 +0300

    Homework

---
+++ homework 2.2.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Яков Контарук on 06.02.2023.
+//
+
+#include <iostream>
+
+int main() {
+    int num1, num2;
+
+    std::cout << "введи число : ";
+    std::cin >> num1;
+    std::cout << "введи еще число: ";
+    std::cin >> num2;
+
+    if (num1 > num2) {
+        std::cout << "Больше" << std::endl;
+    } else if (num1 < num2) {
+        std::cout << "Меньше" << std::endl;
+    } else {
+        std::cout << "Равны" << std::endl;
+    }
+
+    return 0;
+}
