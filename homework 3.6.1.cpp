ebd5d1215be1c901df2861cd84466a03e033dae2
Yakov Kontarook <kontarook@gmail.com>
2023-02-06 19:54:35 +0300

    Homework

---
+++ homework 3.6.1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Яков Контарук on 06.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " x 5 = " << i*5 << endl;
+    }
+    return 0;
+}
