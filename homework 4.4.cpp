2de5cc4a98c31b68a4ccda1913cc1b2d5de0db1a
Yakov Kontarook <kontarook@gmail.com>
2023-02-06 20:07:19 +0300

    Homework

---
+++ homework 4.4.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Яков Контарук on 06.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введите количество элементов в массиве: ";
+    cin >> n;
+    int* arr = new int[n]; // динамич массив
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int min = arr[0], max = arr[0], sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        sum += arr[i];
+    }
+
+    cout << "минимальное значение: " << min << endl;
+    cout << "максимальное значение: " << max << endl;
+    cout << "сумма: " << sum << endl;
+    cout << "среднее: " << (double)sum/n << endl;
+
+    delete[] arr;
+    return 0;
+}
