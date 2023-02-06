53953dc08a47d4398ef5b6921b3d09120c1261b0
Yakov Kontarook <kontarook@gmail.com>
2023-02-06 19:50:46 +0300

    Homework

---
+++ homework 3.5.2.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Яков Контарук on 06.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0;
+    for (int i = 5; i <= 15; i++) {
+        sum += i;
+    }
+    cout << "сумма чисел равна " << sum << endl;
+    return 0;
+}
