4d638fd0e8b5172e757ad6d075847c3968cb609b
Yakov Kontarook <kontarook@gmail.com>
2023-02-07 12:04:09 +0300

    Homework

---
+++ homework 6.4.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Яков Контарук on 07.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    cout << "Введите строку: ";
+    cin >> str;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        str[i] = str[i + 1];
+    }
+
+    cout << "Строка после удаления символа: " << str << endl;
+
+    return 0;
+}
