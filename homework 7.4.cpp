04d19be2f5ce5ee6b6974fd31fdbefb97dae8049
Yakov Kontarook <kontarook@gmail.com>
2023-02-07 12:11:00 +0300

    Homework

---
+++ homework 7.4.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Яков Контарук on 07.02.2023.
+//
+
+#include <iostream>
+using namespace std;
+
+string replaceFirstLast(string input, char symbol) {
+    input[0] = symbol;
+    input[input.length() - 1] = symbol;
+    return input;
+}
+
+int main() {
+    string inputString;
+    char symbol;
+    cout << "Введи слово: ";
+    cin >> inputString;
+    cout << "Введите символ: ";
+    cin >> symbol;
+    cout << "Новое слово: " << replaceFirstLast(inputString, symbol) << endl;
+    return 0;
+}
