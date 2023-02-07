255f0b6b1e011090d07f5bb6a6b2aeb9a2e2ff0f
Yakov Kontarook <kontarook@gmail.com>
2023-02-07 11:56:22 +0300

    Homework

---
+++ homework 5.6.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Яков Контарук on 07.02.2023.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Введите количество строк и столбцов массива: ";
+    cin >> n >> m;
+
+    int arr[n][m];
+
+    cout << "Введите элементы массива: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        sort(arr[i], arr[i] + m);
+    }
+
+    cout << "Отсортированный массив: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for(int i = 0; i < n; i++) {
+        swap(arr[i][0], arr[i][m-1]);
+    }
+    cout << "Массив после замены первого и последнего столбцов: " << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
