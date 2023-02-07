b09f9ee2e934c65e598ca86cb34e8cb9c804cb20
Yakov Kontarook <kontarook@gmail.com>
2023-02-07 12:00:23 +0300

    Homework

---
+++ homework 6.3.2.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Яков Контарук on 07.02.2023.
+//
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    char str[] = "Hello";
+    int n = sizeof(str) / sizeof(str[0]); // определяем размер массива
+
+    // Выводим последний элемент строки
+    cout << "Последним элементом строки: " << str[n-2] << endl;
+
+    // Меняем местами два элемента строки
+    swap(str[1], str[3]);
+
+    cout << "Измененная строка " << str << endl;
+
+    return 0;
+}
---
+++ homework 6.3.3.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Яков Контарук on 07.02.2023.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "Hello";
+    char ch;
+    cout << "Введите символ, который вы хотите удалить: ";
+    cin >> ch;
+
+    int n = str.length();
+    for (int i = 0; i < n; i++) {
+        if (str[i] == ch) {
+            str.erase(i, 1);
+            i--;
+            n--;
+        }
+    }
+
+    cout << "Строка после удаления " << str << endl;
+
+    return 0;
+}
