99df15ef02c9742bd1515bae1c816a87aee254db
Yakov Kontarook <88117408+YakovKontarook@users.noreply.github.com>
2023-07-11 13:32:17 +0300

    main4.py

--- pythonHomework/main4.py
+++ pythonHomework/main4.py
@@ -1,4 +1,12 @@
-number = input("Введите пятизначное число: ")
-digits = list(map(int, number))
-result = digits[1] ** digits[4] * digits[2] / (digits[0] - digits[3])
-print(result)
+number = int(input("Введите пятизначное число: "))
+
+units = number % 10
+tens = (number // 10) % 10
+hundreds = (number // 100) % 10
+thousands = (number // 1000) % 10
+ten_thousands = (number // 10000) % 10
+
+difference = ten_thousands - thousands
+result = ((tens ** units) * hundreds) / difference if difference != 0 else float('nan')
+
+print(f"Результат: {result}")
